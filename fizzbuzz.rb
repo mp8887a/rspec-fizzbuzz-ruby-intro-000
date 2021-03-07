12914d78ded81707ea69f6d476e2b7a7315e4fd0
Mike Pokalsky <mpoksky1989@gmail.com>
2021-03-07 00:49:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,13 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
+  end
   elsif int % 5 == 0
    "Buzz"
+ end
  elsif int % 3 == 0, int % 5 == 0
    "Fizzbuzz"
+ end
  else
    nil
  end
